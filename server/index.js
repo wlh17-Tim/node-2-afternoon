af9989b673c9f173897990d3bd9b28e390c48fde
Timothy Barna <audioclimb@gmail.com>
2020-03-03 16:38:37 -0700

    done

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mc = require('./controllers/messages_controller');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'));
+
+const messagesBaseUrl = "/api/messages";
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
+
+const port = 3001;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
